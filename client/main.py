a4de3d8dde434cee94410a1bd228b07ce552c0d3
DrArzter <chapegarostislav@gmail.com>
2024-03-15 14:33:39 +0100

    Add docstring to most of the functions.

--- client/main.py
+++ client/main.py
@@ -15,6 +15,15 @@ permitRoles = ["Administrator", "Kladovshik", "Injener", "Rukovoditel proizvodst
 
 
 def sanitizeInput(input):
+    """
+    A function that sanitizes the input by removing any forbidden characters.
+    
+    Parameters:
+    input (str): The input string to be sanitized.
+    
+    Returns:
+    str: The sanitized input string.
+    """
     ##TODO: Sanitize input
     forbiddenChars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|', ' ', '(', ')', '$', '%', '&', '卐']
     for char in input:
@@ -24,6 +33,9 @@ def sanitizeInput(input):
     
 
 def initializeDatabase():
+    """
+    Initialize the database.
+    """
     ##TODO: Initialize the database
     cursor = conn.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), password VARCHAR(255), role VARCHAR(255) NULL, last_login DATETIME, last_logout DATETIME, last_login_device VARCHAR(255) NULL)')
@@ -35,6 +47,16 @@ def initializeDatabase():
 
 
 def registration(name, password):
+    """
+    Register a new user in the system.
+
+    Parameters:
+    name (str): The name of the user to be registered.
+    password (str): The password of the user to be registered.
+
+    Returns:
+    bool: True if the user was successfully registered, False if the user already exists.
+    """
     ##TODO: Register a new user
     cursor.execute('SELECT * FROM users WHERE name = ?', (name,))
     if cursor.fetchone():
@@ -48,11 +70,25 @@ def registration(name, password):
     
 
 def hash_password(password):
+    """
+    Hash a password using sha256 algorithm.
+
+    Parameters:
+    password (str): The password to be hashed.
+
+    Returns:
+    str: The hashed password.
+    """
     ##TODO: Hash a password
     return hashlib.sha256(password.encode()).hexdigest()
 
 
 def checkRole(user_id):
+    """
+    Check role of a user    
+    :param user_id: The ID of the user to check the role for
+    :return: The role of the user
+    """
     ##TODO: Check role of a user
     cursor.execute('SELECT role FROM users WHERE id = ?', (user_id,))
     role = cursor.fetchone()[0]
@@ -60,6 +96,17 @@ def checkRole(user_id):
 
 
 def login(name, password, device):
+    """
+    Login a user.
+
+    Parameters:
+    - name (str): The name of the user.
+    - password (str): The password of the user.
+    - device (str): The device used for login.
+
+    Returns:
+    - user (dict): The user information if login is successful, False otherwise.
+    """
     ##TODO: Login a user
     hashed_password = hash_password(password)
     cursor.execute('SELECT * FROM users WHERE name = ? AND password = ?', (name, hashed_password))
@@ -74,6 +121,15 @@ def login(name, password, device):
 
 
 def logout(user_id):
+    """
+    Logout a user by updating the last_logout timestamp in the users table for the given user_id.
+    
+    Args:
+        user_id: The unique identifier of the user to be logged out.
+    
+    Returns:
+        bool: True if the user is successfully logged out, False otherwise.
+    """
     ##TODO: Logout a user
     cursor.execute('UPDATE users SET last_logout = DATETIME() WHERE id = ?', (user_id,))
     conn.commit()
@@ -81,6 +137,15 @@ def logout(user_id):
 
 
 def changeName(admin_id, target_user_id, new_name):
+    """
+    Change name of a user
+    Parameters:
+    - admin_id: int, the ID of the administrator performing the change
+    - target_user_id: int, the ID of the user whose name will be changed
+    - new_name: str, the new name to assign to the user
+    Returns:
+    - bool, True if the name was successfully changed, False otherwise
+    """
     #TODO: Change name of a user
     if checkRole(admin_id) == 'Administrator':
         cursor.execute('UPDATE users SET name = ? WHERE id = ?', (new_name, target_user_id))
@@ -92,6 +157,17 @@ def changeName(admin_id, target_user_id, new_name):
     
 
 def changeRole(admin_id, target_user_id, role):
+    """
+    Change role of a user.
+
+    Parameters:
+    - admin_id (int): The user ID of the administrator performing the role change.
+    - target_user_id (int): The user ID of the user whose role is being changed.
+    - role (str): The new role to assign to the user.
+
+    Returns:
+    - bool: True if the role change was successful, False otherwise.
+    """
     #TODO: Change role of a user
     if checkRole(admin_id) == 'Administrator':
         cursor.execute('UPDATE users SET role = ? WHERE id = ?', (role, target_user_id))
@@ -103,6 +179,17 @@ def changeRole(admin_id, target_user_id, role):
     
 
 def changePassword(admin_id, target_user_id, new_password):
+    """
+    Change password of a user
+
+    Args:
+        admin_id: The ID of the administrator performing the password change
+        target_user_id: The ID of the user whose password is being changed
+        new_password: The new password for the user
+
+    Returns:
+        bool: True if the password was successfully changed, False otherwise
+    """
     #TODO: Change password of a user
     if checkRole(admin_id) == 'Administrator':
         cursor.execute('UPDATE users SET password = ? WHERE id = ?', (hash_password(new_password), target_user_id))
@@ -114,6 +201,16 @@ def changePassword(admin_id, target_user_id, new_password):
 
 
 def deleteUser(admin_id, target_user_id):
+    """
+    Delete a user if the admin is an administrator and the target user is not an administrator.
+    
+    Parameters:
+    - admin_id: the id of the admin user
+    - target_user_id: the id of the user to be deleted
+    
+    Returns:
+    - True if the user is successfully deleted, False otherwise
+    """
     #TODO: Delete a user
     if checkRole(admin_id) == 'Administrator' and checkRole(target_user_id) != 'Administrator':
         cursor.execute('DELETE FROM users WHERE id = ?', (target_user_id,))
@@ -125,6 +222,14 @@ def deleteUser(admin_id, target_user_id):
     
 
 def getUsers(admin_id):
+    """
+    Get all users using the given admin ID and return the list of users.
+    Parameters:
+    - admin_id: string, the ID of the administrator
+    Returns:
+    - list of lists, each inner list representing a user's data
+    - False if the admin ID does not have the 'Administrator' role
+    """
     #TODO: Get all users
     if checkRole(admin_id) == 'Administrator':
         cursor.execute('SELECT * FROM users')
@@ -136,6 +241,15 @@ def getUsers(admin_id):
     
     
 def getComponents(user_id):
+    """
+    Get all components based on the user ID if the user's role is permitted, otherwise return False.
+    
+    Parameters:
+    user_id (int): The ID of the user to check permissions for.
+    
+    Returns:
+    list or bool: A list of components if the user's role is permitted, False otherwise.
+    """
     #TODO: Get all components
     if checkRole(user_id) in permitRoles:
         cursor.execute('SELECT * FROM components')
@@ -147,6 +261,13 @@ def getComponents(user_id):
     
     
 def changeComponentName(user_id, component_id, new_name):
+    """
+    Change name of a component
+    user_id: int, the user's ID
+    component_id: int, the component's ID
+    new_name: str, the new name for the component
+    Returns True if the component name was successfully changed, False otherwise
+    """
     #TODO: Change name of a component
     if checkRole(user_id) in permitRoles:
         cursor.execute('UPDATE components SET name = ? WHERE id = ?', (new_name, component_id))
@@ -158,6 +279,17 @@ def changeComponentName(user_id, component_id, new_name):
 
 
 def changeComponentArticle(user_id, component_id, new_article):
+    """
+    Change article of a component.
+
+    Parameters:
+    - user_id: int, the user ID
+    - component_id: int, the component ID
+    - new_article: str, the new article
+
+    Returns:
+    - bool: True if the article was successfully changed, False otherwise
+    """
     #TODO: Change article of a component
     if checkRole(user_id) in permitRoles:
         cursor.execute('UPDATE components SET article = ? WHERE id = ?', (new_article, component_id))
@@ -169,6 +301,17 @@ def changeComponentArticle(user_id, component_id, new_article):
     
     
 def changeComponentType(user_id, component_id, new_type):
+    """
+    Change type of a component.
+
+    Parameters:
+    user_id (int): The user ID.
+    component_id (int): The component ID.
+    new_type (str): The new type to assign to the component.
+
+    Returns:
+    bool: True if the type of the component was successfully changed, False otherwise.
+    """
     #TODO: Change type of a component
     if checkRole(user_id) in permitRoles:
         cursor.execute('UPDATE components SET type = ? WHERE id = ?', (new_type, component_id))
@@ -180,6 +323,15 @@ def changeComponentType(user_id, component_id, new_type):
     
     
 def changeComponentWeight(user_id, component_id, new_weight):
+    """
+    Change weight of a component
+    Parameters:
+    - user_id: the id of the user making the change
+    - component_id: the id of the component to change
+    - new_weight: the new weight to set for the component
+    Returns:
+    - True if the weight change is successful, False otherwise
+    """
     #TODO: Change weight of a component
     if checkRole(user_id) in permitRoles:
         cursor.execute('UPDATE components SET weight = ? WHERE id = ?', (new_weight, component_id))
@@ -191,6 +343,17 @@ def changeComponentWeight(user_id, component_id, new_weight):
     
     
 def changeComponentProductionDate(user_id, component_id, new_production_date):
+    """
+    Change production date of a component.
+
+    Parameters:
+    - user_id (int): The user ID attempting to change the production date.
+    - component_id (int): The ID of the component whose production date is to be changed.
+    - new_production_date (str): The new production date to be set for the component.
+
+    Returns:
+    - bool: True if the production date was successfully updated, False otherwise.
+    """
     #TODO: Change production date of a component
     if checkRole(user_id) in permitRoles:
         cursor.execute('UPDATE components SET production_date = ? WHERE id = ?', (new_production_date, component_id))
@@ -202,6 +365,12 @@ def changeComponentProductionDate(user_id, component_id, new_production_date):
     
     
 def getCerteinComponents(user_id, component_type):
+    """
+    Get certain components based on the user ID and component type.
+    :param user_id: the ID of the user
+    :param component_type: the type of component to retrieve
+    :return: a list of components if the user's role permits, otherwise False
+    """
     #TODO: Get certain components
     if checkRole(user_id) in permitRoles:
         cursor.execute('SELECT * FROM components WHERE type = ?', (component_type,))
